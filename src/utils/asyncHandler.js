a621c673699e5cf839f28df979217031282fdd2b
patelzeel044 <patelzeel044@gmail.com>
2024-07-30 19:16:22 +0530

    change asyncHandler and startscript

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,33 @@
-const asyncHandler = (requestHandler)=>{
-  return  (req,res,next)=>{
-        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
-    }
-}
+const asyncHandler = (requestHandler) => async(req, res, next) => {
+       try {
+           await requestHandler(req, res, next)
+       } catch (error) {
+        const statusCode = error.statusCode || 500;
+        const errorMessage = error.message || "Internal Server Error";
+        res.status(statusCode).json({
+            success: false,
+            error: errorMessage
+        });
+       }
+   }
 
-export {asyncHandler}
+   export { asyncHandler };
+
+
+
+   
+
+   /* const asyncHandler = (requestHandler) => {
+    return async (req, res, next) => {
+        try {
+            await Promise.resolve(requestHandler(req, res, next));
+        } catch (error) {
+            const statusCode = error.statusCode || 500;
+            const errorMessage = error.message || "Internal Server Error";
+            res.status(statusCode).json({
+                success: false,
+                error: errorMessage
+            })
+        }
+     }
+   } */
