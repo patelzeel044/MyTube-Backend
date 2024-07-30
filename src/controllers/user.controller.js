62a37b2e9713657f23a4d681abe39d06b1a146e6
patelzeel044 <patelzeel044@gmail.com>
2024-07-30 16:35:08 +0530

    cookie option change

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -123,6 +123,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
+    sameSite: "None"
   };
 
   return res
@@ -158,6 +159,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
+    sameSite: "None"
   };
 
   return res
@@ -194,6 +196,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
       const options = {
         httpOnly: true,
         secure: true,
+        sameSite: "None"
       };
 
       const { accessToken, newRefreshToken } =
